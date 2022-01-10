04230ba7b68f6b8552dc573314a743e70622d4e9
Miguel Vidal <68684733+migvidal@users.noreply.github.com>
2022-01-10 16:09:06 +0100

    header template

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+
+import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import { initializeApp } from "firebase/app";
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
-firebase.initializeApp({
 
-});
+// Import the functions you need from the SDKs you need
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDjxA1hOfiPMTCdbUZQGtEitQs8KUNIRu4",
+  authDomain: "micro-chat-bc474.firebaseapp.com",
+  projectId: "micro-chat-bc474",
+  storageBucket: "micro-chat-bc474.appspot.com",
+  messagingSenderId: "829001232661",
+  appId: "1:829001232661:web:17a42f3b500b04c733791c"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+const auth = getAuth();
 
 function App() {
+  const [user] = useAuthState(auth);
   return (
     <div className="App">
       <header className="App-header">
+        {!!user ?
+          <strong>{user.displayName}</strong>
+          : <button>Log In</button>
+        }
       </header>
+      <div className="App-main">
+      </div>
     </div>
   );
 }
