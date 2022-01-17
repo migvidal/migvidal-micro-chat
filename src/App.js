2b525119cbd137243b89fbdc21f75f42322129b7
Miguel Vidal <68684733+migvidal@users.noreply.github.com>
2022-01-17 17:42:14 +0100

    debugging auth error

--- src/App.js
+++ src/App.js
@@ -56,7 +56,9 @@ function App() {
 function SignIn() {
   const authWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithRedirect(provider);
+    auth.signInWithRedirect(provider)
+      .then(m => console.log('success:' + m))
+      .catch(e => console.log('error:' + e));
   }
 
   return (
